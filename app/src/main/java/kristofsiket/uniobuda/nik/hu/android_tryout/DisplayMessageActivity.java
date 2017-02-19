b6a7be9f25b9d82288f4d80cd984053e21631cef
kristof-siket <kristofsiket@gmail.com>
2017-02-19 22:27:33 +0100

    DisplayMessageActivity created, the two activities linked.

---
+++ app/src/main/java/kristofsiket/uniobuda/nik/hu/android_tryout/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package kristofsiket.uniobuda.nik.hu.android_tryout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
