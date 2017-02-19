3eee151a29b56a22727a3e87eb99616fa7da2078
kristof-siket <kristofsiket@gmail.com>
2017-02-19 16:54:32 +0100

    Init commit made, the repo has been set up.

---
+++ app/src/main/java/kristofsiket/uniobuda/nik/hu/android_tryout/MainActivity.java
@@ -0,0 +1,13 @@
+package kristofsiket.uniobuda.nik.hu.android_tryout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
