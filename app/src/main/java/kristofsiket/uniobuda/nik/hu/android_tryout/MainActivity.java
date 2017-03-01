3d499eec770adc51dc8209bb42283f594a5879df
kristof-siket <kristofsiket@gmail.com>
2017-03-01 18:10:40 +0100

    An AlertDialog was added to onStart().

--- app/src/main/java/kristofsiket/uniobuda/nik/hu/android_tryout/MainActivity.java
+++ app/src/main/java/kristofsiket/uniobuda/nik/hu/android_tryout/MainActivity.java
@@ -1,10 +1,15 @@
 package kristofsiket.uniobuda.nik.hu.android_tryout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import static android.content.DialogInterface.*;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "kristofsiket.uniobuda.nik.hu.android_tryout.MESSAGE";
@@ -21,5 +26,47 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+
+    @Override
+    protected void onStart() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title)
+                .setNeutralButton(R.string.cancel, new OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                } );
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
